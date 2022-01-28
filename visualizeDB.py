d74dde4c98d19581d983ebc9b207448d36230541
RBaptista <riniltton11@gmail.com>
2022-01-28 16:03:41 -0100

    Update visualizeDB.py

--- visualizeDB.py
+++ visualizeDB.py
@@ -3,7 +3,6 @@ import pandas as pd
 equipamentos = pd.read_sql_table('equipamentos','sqlite:///cio.db')
 requisicoes = pd.read_sql_table('requisicoes','sqlite:///cio.db')
 i =0
-j =0
 while equipamentos.values[i][3]:
     if equipamentos.values[i][0] in requisicoes:
         print(equipamentos.values[i])
