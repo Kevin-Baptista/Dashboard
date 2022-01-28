f634c3b327f4db18b4cc83d132c1799f88c334b9
RBaptista <riniltton11@gmail.com>
2022-01-28 11:15:00 -0100

    Populating database

--- visualizeDB.py
+++ visualizeDB.py
@@ -2,5 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mysql.connector
 
-mydb=mysql.connector.connect(host=”localhost”,user=”root”,password=”Your_Password”,database=”Database_Name”)
+mydb=mysql.connector.connect(host='localhost',user='root',password='',database='cio')
 mycursor=mydb.cursor()
+
+
+mycursor.execute("select * from equipamentos")
+result = mycursor.fetchall
